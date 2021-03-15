10ef7da00f7930782ea04dc0b496f3349f02f63c
Eric <duoyuan_he@outlook.com>
2021-03-15 17:37:43 +0800

    upload my hw0

---
+++ hw0/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+import java.util.Arrays;
+public class Main {
+    /** return the world */
+    public static void windowPosSum(int[] a, int n) {
+
+    }
+    public static void main(String[] args) {
+        String[] a = {"cat", "dog", "laser horse", "ketchup", "horse", "horbse"};
+
+        for (String characters: a){
+            System.out.println(characters);
+        }
+    }
+}
