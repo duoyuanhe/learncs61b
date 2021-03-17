2c5c7ab44484ee2fac76e8b02eb38704631dd90b
Eric <duoyuan_he@outlook.com>
2021-03-17 11:10:42 +0800

    project 0

---
+++ proj0/Body.java
@@ -0,0 +1,39 @@
+import org.w3c.dom.ls.LSOutput;
+
+public class Body {
+
+    public double xxPos;
+    public double yyPos;
+    public double xxVel;
+    public double yyVel;
+    public double mass;
+    public String imgFileName;
+
+    public Body(double xP, double yP, double xV,
+                double yV, double m, String img) {
+
+        xxPos = xP;
+        yyPos = yP;
+        xxVel = xV;
+        yyVel = yV;
+        mass = m;
+        imgFileName = img;
+    }
+
+    public Body(Body b){
+        this.xxPos = b.xxPos;
+        this.yyPos = b.yyPos;
+        this.xxVel = b.xxVel;
+        this.yyVel = b.yyVel;
+        this.mass = b.mass;
+        this.imgFileName = b.imgFileName;
+    }
+
+    public double calcDistance(Body b2){
+        return Math.sqrt(Math.pow(this.xxPos - b2.xxPos, 2) +
+                Math.pow(this.yyPos - b2.yyPos, 2));
+    }
+
+
+
+}
